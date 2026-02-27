dd79b75c98e5284cb564bf268572e090b7d1141a
Prity Sarkar <99pritys@gmail.com>
2026-02-27 11:35:00 +0530

    Delete src directory

--- src/constants/filterOptions.ts
+++
@@ -1,6 +0,0 @@
-// constants/filterOptions.ts
-export const FILTER_OPTIONS = {
-  sizes: ["S", "M", "L", "XL"],
-  colors: ["Black", "White", "Blue", "Red"],
-  brands: ["Nike", "Adidas", "Puma"],
-};
